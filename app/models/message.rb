892800f2dd7093686ffa6dbf6bc51ebb7ad31ee9
Tommy Hunke <thunke@decisionstreet.com>
2015-03-21 12:43:50 -0400

    Return a Caesar Shift or Reverse Alphabet cipher every now and then

--- app/models/message.rb
+++ app/models/message.rb
@@ -11,19 +11,55 @@
 
 class Message < ActiveRecord::Base
 
+    ALPHABET = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
     def encrypt
         arr = self.message.upcase.split(//)
-        alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-                     "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        a2 = alphabet.clone
         subs = []
 
+        # return a caesar shift or a reverse alphabet cipher once in a blue moon
+        n = Random.rand (1..100)
+        if n == 1
+            subs = caesarShift
+        elsif n == 2
+          subs = reverseAlphabet
+        else
+           subs = simpleSub
+        end
+
+        arr.map { |l| ALPHABET.index(l).nil? ? l : subs[ALPHABET.index l] }.join
+    end
+
+
+    protected
+    def simpleSub
+        subs = []
+        a2 = ALPHABET.clone
         while a2.length > 0 do
             n = Random.rand (a2.length)
             subs.push(a2[n])
             a2.delete_at(n)
         end
+        subs
+    end
 
-        arr.map { |l| alphabet.index(l).nil? ? l : subs[alphabet.index l] }.join
+
+    protected
+    def caesarShift
+        subs = []
+        shift = Random.rand (1..ALPHABET.length)
+        i = 0
+        while i < ALPHABET.length  do
+            s = i + shift
+            subs.push(ALPHABET[s > 25 ? s - 26 : s])
+            i +=1
+        end
+        subs
+    end
+
+    protected
+    def reverseAlphabet
+        return ALPHABET.reverse
     end
 end
